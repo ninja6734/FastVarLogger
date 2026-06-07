edea4c426e74f2ac5146f4285d71c16778095e7b
Dennis Skurka <dennis.skurka@gmail.com>
2026-06-07 02:17:45 +0200

    Implement connection feature and update UI for ConnectTab and TabBar

--- my-app/backend/main.go
+++ my-app/backend/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"net/http"
 
+	"fmt"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+type ConnectRequest struct {
+	IP string `json:"ip"`
+}
 
 func main() {
 	r := gin.Default()
@@ -22,6 +28,23 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hallo vom Go-Backend!",
 		})
+	})
+
+	r.POST("/api/connect", func(c *gin.Context) {
+		var req ConnectRequest
+		fmt.Println("Received connection request")
+		fmt.Println("Request body: ", c.Request.Body)
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Ungültige Anfrage"})
+			return
+		}
+
+		// Hier kannst du die Logik implementieren, um eine Verbindung zum Roboter herzustellen
+		// Zum Beispiel: err := connectToRobot(req.IP)
+
+		fmt.Println("Connecting to: ", req.IP)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Verbindung zum Roboter hergestellt"})
 	})
 
 	r.Run(":8080") // läuft auf http://localhost:8080
--- my-app/frontend/src/modules/TabBar/TabBar.tsx
+++ my-app/frontend/src/modules/TabBar/TabBar.tsx
@@ -8,12 +8,12 @@ function TabBar() {
 
     return(
         <div>
-            <div>
-                <button className="tabBtn" onClick={() => setActiveTab("Connect")}>Connect</button>
-                <button className="tabBtn" onClick={() => setActiveTab("Raw")}>Raw Data</button>
-                <button className="tabBtn" onClick={() => setActiveTab("Graphic")}>Graphical Data</button>
+            <div className="flex gap-4 p-4 bg-amber-500">
+                <button className="px-4 py-2 border-2 border-black rounded-2xl hover:bg-amber-100 bg-amber-50" onClick={() => setActiveTab("Connect")}>Connect</button>
+                <button className="px-4 py-2 border-2 border-black rounded-2xl hover:bg-amber-100 bg-amber-50" onClick={() => setActiveTab("Raw")}>Raw Data</button>
+                <button className="px-4 py-2 border-2 border-black rounded-2xl hover:bg-amber-100 bg-amber-50 " onClick={() => setActiveTab("Graphic")}>Graphical Data</button>
             </div>
-            <div>
+            <div className="p-4 bg-amber-100">
                 {activeTab === 'Connect' && <ConnectContent />}
                 {activeTab === 'Raw' && <RawContent />}
                 {activeTab === "Graphic" && <GraphicContent/>}
