04235f44dde390f2d52d556baf2779aa0e12fed5
Dennis Skurka <dennis.skurka@gmail.com>
2026-06-25 22:58:09 +0200

    Implement UDP data encoding and decoding functions

--- my-app/backend/modules/UDPConnectionCodec/UDPConnectionCodec.go
+++ my-app/backend/modules/UDPConnectionCodec/UDPConnectionCodec.go
@@ -26,3 +26,17 @@ func CreateUDPDataPacket(data []byte) []byte {
 	packet = append(packet, data...)                           // data
 	return packet
 }
+
+func DecodeUDPData(data []byte) ([]byte, error) {
+	if len(data) < DataSize+4 || string(data[:4]) != MagicNumber {
+		return nil, fmt.Errorf("Invalid UDP data")
+	}
+	return data[4:], nil
+}
+
+func EncodeUDPData(data []byte) ([]byte, error) {
+	packet := make([]byte, 0)
+	packet = append(packet, []byte{0x46, 0x56, 0x4c, 0x52}...) // magic number
+	packet = append(packet, data...)
+	return packet, nil
+}
